0607b6379aba48383840a62998f5053f11754a6f
iwrawww@gmail.com <iwrawww@gmail.com>
2025-05-02 15:34:26 +0700

    загрузим раз

---
+++ FileInfo.cs
@@ -0,0 +1,8 @@
+﻿public class FileInfo
+{
+  public string Name { get; set; }
+  public string Path { get; set; }
+  public DateTime LastModified { get; set; }
+  public long Size { get; set; }
+  public string Hash { get; set; }
+}
---
+++ SyncLog.cs
@@ -0,0 +1,6 @@
+﻿public class SyncLog
+{
+  public DateTime SyncTime { get; set; }
+  public List<FileInfo> Files { get; set; } = new List<FileInfo>();
+  public List<string> Actions { get; set; } = new List<string>();
+}
