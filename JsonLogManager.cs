83937562cab437a8d2f5890023a8ea9b70073080
iwrawww@gmail.com <iwrawww@gmail.com>
2025-05-02 15:35:28 +0700

    куча файлов уже

---
+++ JsonLogManager.cs
@@ -0,0 +1,26 @@
+﻿public class JsonLogManager : ILogManager
+{
+  private readonly string _logFilePath;
+
+  public JsonLogManager(string logFilePath)
+  {
+    _logFilePath = logFilePath;
+  }
+
+  public void SaveLog(SyncLog log)
+  {
+    string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+    File.WriteAllText(_logFilePath, json);
+  }
+
+  public SyncLog LoadLog()
+  {
+    if (!File.Exists(_logFilePath))
+    {
+      return new SyncLog();
+    }
+
+    string json = File.ReadAllText(_logFilePath);
+    return JsonConvert.DeserializeObject<SyncLog>(json);
+  }
+}
---
+++ XmlLogManager.cs
@@ -0,0 +1,32 @@
+﻿public class XmlLogManager : ILogManager
+{
+  private readonly string _logFilePath;
+
+  public XmlLogManager(string logFilePath)
+  {
+    _logFilePath = logFilePath;
+  }
+
+  public void SaveLog(SyncLog log)
+  {
+    var serializer = new XmlSerializer(typeof(SyncLog));
+    using (var writer = new StreamWriter(_logFilePath))
+    {
+      serializer.Serialize(writer, log);
+    }
+  }
+
+  public SyncLog LoadLog()
+  {
+    if (!File.Exists(_logFilePath))
+    {
+      return new SyncLog();
+    }
+
+    var serializer = new XmlSerializer(typeof(SyncLog));
+    using (var reader = new StreamReader(_logFilePath))
+    {
+      return (SyncLog)serializer.Deserialize(reader);
+    }
+  }
+}
